3e07c5e88c38469fa74e7d7e788ab9e37a3f18a2
cr7upy <upytechno@gmail.com>
2023-07-29 10:35:01 +0530

    first commit yt

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "24a6b153acmsh331da384778626dp1b846djsnd4b0f10c24e8",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
